cb25ebd2e39141a528833fff5c7caa90670279fd
Ed <edglxy@gmail.com>
2024-08-21 17:47:14 +0800

    Component Prop

---
+++ resources/views/components/flash-message.blade.php
@@ -0,0 +1,3 @@
+@props(['msg', 'bg' => 'bg-green-500'])
+
+<p class="text-sm font-medium text-white {{ $bg }} px-3 py-rounded-md">{{ $msg }}</p>
--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -5,15 +5,14 @@
     <div class="card mb-4">
         <h2 class="font-bold mb-4">Create a new post</h2>
 
+
         <form action="{{ route('posts.store') }}" method="post">
             @csrf
 
             {{-- Session Messages --}}
             @if (session('success'))
-            <div>
-                <p>
-                    {{ session('success') }}
-                </p>
+            <div class="mb-2">
+                <x-flash-message msg="{{ session('success') }}" bg="bg-yellow-500"></x-flash-message>
             </div>
             @endif
 
